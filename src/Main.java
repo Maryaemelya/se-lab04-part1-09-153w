fd9c1fda6581857eca4e9cfd6213c140d2302299
FkishDaniels <irina2808777@gmail.com>
2023-03-10 11:10:53 +0300

    Marukha_V10

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
         try {
             switch (args[0].toLowerCase()) {
                 case "v0" -> System.out.println(V0.func(Double.parseDouble(args[1])));
                 case "v13" -> System.out.println(V13.func(Double.parseDouble(args[1])));
+                case "v10" -> System.out.println(v10.func(args[1],args[2],args[3]));
                 case "exit" -> {
                     System.out.println("Done");
                     return;
