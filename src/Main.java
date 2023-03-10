5ad37cb52089c9cfb44d39f0f9cdede7c471d4e0
Maryaemelya <maryemelya2001@gmail.com>
2023-03-10 11:27:52 +0300

    V4

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         try {
             switch (args[0].toLowerCase()) {
                 case "v0" -> System.out.println(V0.func(Double.parseDouble(args[1])));
+                case "v4" -> System.out.println(V4.func(Double.parseDouble(args[1]), Double.parseDouble(args[2])));
                 case "v13" -> System.out.println(V13.func(Double.parseDouble(args[1])));
                 case "v10" -> System.out.println(v10.func(args[1],args[2],args[3]));
                 case "exit" -> {
---
+++ src/V4.java
@@ -0,0 +1,5 @@
+public class V4 {
+    public static double func (double a, double x){
+        return Math.log(Math.abs(Math.pow(a,7))) + Math.atan(x*x) + Math.PI/Math.sqrt(Math.abs(a+x));
+    }
+}
